42675f165c08640275da0f629bca61e50a13ee94
Sam A. Horvath-Hunt <hello@samhh.com>
2021-01-20 13:47:22 +0000

    Display distinct git ref colour if diverged from remote
    This assumes that the relevant remote is called "origin".

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -6,11 +6,15 @@ function show_git_status
   if command git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null
     set -l dirty (command git status -s --ignore-submodules=dirty 2>/dev/null | wc -l | sed -e 's/^ *//' -e 's/ *$//')
     set -l ref (command git symbolic-ref --short HEAD 2> /dev/null; or command git rev-parse --short HEAD 2>/dev/null)
+    set -l local (command git rev-parse "$ref")
+    set -l remote (command git rev-parse "origin/$ref")
 
-    if [ "$dirty" = "0" ]
-      prompt_segment normal green "$ref"
-    else
+    if [ "$dirty" != "0" ]
       prompt_segment normal red "$ref"
+    else if [ "$local" != "$remote" ]
+      prompt_segment normal magenta "$ref"
+    else
+      prompt_segment normal green "$ref"
     end
 
     spacer
