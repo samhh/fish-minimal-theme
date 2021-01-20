6b36286e8a33462a2098be1697f15d0e9e3bf125
Sam A. Horvath-Hunt <hello@samhh.com>
2021-01-20 13:24:18 +0000

    Fix prompt stderr not redirected to /dev/null

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -3,9 +3,9 @@ function show_pwd
 end
 
 function show_git_status
-  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
-    set -l ref (command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
+  if command git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null
+    set -l dirty (command git status -s --ignore-submodules=dirty 2>/dev/null | wc -l | sed -e 's/^ *//' -e 's/ *$//')
+    set -l ref (command git symbolic-ref --short HEAD 2> /dev/null; or command git rev-parse --short HEAD 2>/dev/null)
 
     if [ "$dirty" = "0" ]
       prompt_segment normal green "$ref"
@@ -14,7 +14,7 @@ function show_git_status
     end
 
     spacer
-   end
+  end
 end
 
 function fish_right_prompt
