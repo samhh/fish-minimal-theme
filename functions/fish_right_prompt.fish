4140b8856be338dfc5630bb1a6250b69751b6fda
Sam A. Horvath-Hunt <hello@samhh.com>
2021-01-21 11:53:05 +0000

    Fix error if remote branch doesn't exist

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -5,9 +5,9 @@ end
 function show_git_status
   if command git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null
     set -l dirty (command git status -s --ignore-submodules=dirty 2>/dev/null | wc -l | sed -e 's/^ *//' -e 's/ *$//')
-    set -l ref (command git symbolic-ref --short HEAD 2> /dev/null; or command git rev-parse --short HEAD 2>/dev/null)
+    set -l ref (command git symbolic-ref --short HEAD 2>/dev/null; or command git rev-parse --short HEAD 2>/dev/null)
     set -l local (command git rev-parse "$ref")
-    set -l remote (command git rev-parse "origin/$ref")
+    set -l remote (command git rev-parse "origin/$ref" 2>/dev/null)
 
     if [ "$dirty" != "0" ]
       prompt_segment normal red "$ref"
