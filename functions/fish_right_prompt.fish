6482d858bf5da520c19b27b484d1f2c582c9f193
Sam A. Horvath-Hunt <hello@samhh.com>
2021-07-11 13:02:11 +0100

    Differentiate between dirty dir and dirty repo as a whole

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -2,15 +2,22 @@ function show_pwd
   prompt_segment normal white (prompt_pwd)
 end
 
+function dirty_files_in_dir
+  command git status $argv[1] -s --ignore-submodules=dirty 2>/dev/null | wc -l | sed -e 's/^ *//' -e 's/ *$//'
+end
+
 function show_git_status
   if command git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null
-    set -l dirty (command git status -s --ignore-submodules=dirty 2>/dev/null | wc -l | sed -e 's/^ *//' -e 's/ *$//')
+    set -l dirty_dir (dirty_files_in_dir ".")
+    set -l dirty_repo (dirty_files_in_dir ":/")
     set -l ref (command git symbolic-ref --short HEAD 2>/dev/null; or command git rev-parse --short HEAD 2>/dev/null)
     set -l local (command git rev-parse "$ref" 2>/dev/null)
     set -l remote (command git rev-parse "origin/$ref" 2>/dev/null)
 
-    if [ "$dirty" != "0" ]
+    if [ "$dirty_dir" != "0" ]
       prompt_segment normal red "$ref"
+    else if [ "$dirty_repo" != "0" ]
+      prompt_segment normal yellow "$ref"
     else if [ "$local" != "$remote" ]
       prompt_segment normal magenta "$ref"
     else
